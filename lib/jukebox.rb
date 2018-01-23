408b8cfeadeba8150fd4338708a27b123c76f23d
York Wang <yw544@nyu.edu>
2018-01-23 05:03:23 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,15 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  help_hash = {
+    "help" => "displays this help message",
+    "list" => "displays a list of songs you can play",
+    "play" => "lets you choose a song to play",
+    "exit" => "exits this program"
+  }
+puts "I accept the following commands:"
+help_hash.each do |command, action|
+  puts "- #{command} : #{action}"
+end
+end
