1b92333c5aeb76dab1d1c114ed4f0b0b06a940ab
York Wang <yw544@nyu.edu>
2018-01-23 07:25:38 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,3 +22,45 @@ help_hash.each do |command, action|
   puts "- #{command} : #{action}"
 end
 end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{num}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs[input.to_i-1]}"
+  elsif songs.include?(input)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  command = nil
+  while command != "exit"
+    puts "Please enter a command:"
+    command = gets.chomp
+
+    if command == "help"
+      help
+    elsif command == "play"
+      play
+    elsif command == "list"
+      list
+    elsif command == "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
+  
